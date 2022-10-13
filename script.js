328c34bb7bfb160eed8b597945564dfae4494dc2
Wil-Wes <will.d.wes@gmail.com>
2022-10-13 09:48:22 -0500

    CompletedDataStructuresExercise

---
+++ script.js
@@ -0,0 +1,86 @@
+// 1a
+const jedi = [];
+
+// 1b
+jedi[0] = `Luke`;
+console.log(jedi);
+
+// 1c
+jedi.push(`Obi-Wan Kenobi`);
+console.log(jedi);
+
+// 1d
+jedi.unshift(`Yoda`);
+console.log(jedi);
+
+// 1e
+console.log(jedi[1]);
+
+// 1f
+jedi.splice(2);
+console.log(jedi);
+
+// 1g
+const yoda = jedi.shift();
+
+// 2a
+const sithLords = [`Darth Vader`,`Darth Sidious`,`Darth Maul`];
+
+// 2b 
+const imperialOfficers = [`Grand Moff Tarkin`, `Orson Krennic`];
+
+// 2c
+
+const starWarsVillains = sithLords.concat(imperialOfficers);
+console.log(starWarsVillains);
+
+// 2d
+console.log(starWarsVillains.slice(0, 2));
+
+// 3a
+const droids = {
+    astromech: `R2-D2`,
+    protocol: `C-3PO`,
+    assassin: `IG-88`
+};
+console.log(droids);
+
+// 3b
+console.log(droids [`astromech`]);
+
+// 3c
+console.log(droids.protocol);
+
+// 3d
+droids.assassin = `IG-11`;
+console.log(droids.assassin);
+
+// BONUS
+// 4
+console.log(starWarsVillains[0].slice(6, 7));
+
+// 5
+console.log(sithlords.slice(-2, -1));
+
+// 6a
+const starWarsMovies = [
+    {
+        episodeOne: `The Phantom Menace`,
+        episodeTwo: `Attack of the Clones`,
+        episodeThree: `Revenge of the Sith`
+    },
+    {
+        episodeFour: "A New Hope", 
+        episodeFive: "The Empire Strikes Back",
+        episodeSix: "Return of the Jedi"
+    },
+    {
+        episodeSeven: "The Force Awakens", 
+        episodeEight: "The Last Jedi", 
+        episodeNine: "The Rise of Skywalker"
+    }
+];
+
+// 6b
+starWarsMovies.splice(1, 0, `Solo`, `Rogue One`);
+console.log(starWarsMovies);
